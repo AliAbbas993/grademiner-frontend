cad7eff566dafb5e4eb8bd4c1e5de8c7ac4377d4
Ali Abbas <imvskp@hotmail.com>
2021-07-30 18:38:47 +0500

    menu changes added

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,10 @@
-<?php echo 'footer'?>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <script src="./assets/js/jquery-3.3.1.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+    <script src="./assets/js/main.js"></script>
+  </body>
+</html>
