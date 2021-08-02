0a672c5d507f1029d35c8ea73ac72ccf9492f79b
Ali Abbas <imvskp@hotmail.com>
2021-08-02 17:32:44 +0500

    pricalculator section added

--- includes/header.php
+++ includes/header.php
@@ -14,69 +14,71 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle">
-              <svg width="20px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-2x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>
-          </span>
+    <div class="header-block">
+      <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle">
+                <svg width="20px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="svg-inline--fa fa-times fa-w-11 fa-2x"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class=""></path></svg>
+            </span>
+          </div>
         </div>
+        <div class="site-mobile-menu-body"></div>
       </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-    
-    <header class="site-navbar" role="banner">
+      
+      <header id="site-navbar" class="site-navbar" role="banner">
 
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-11 col-xl-2">
-            <h1 class="mb-0 site-logo"><a href="index.html" class="text-white mb-0">Brand</a></h1>
-          </div>
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
+        <div class="container">
+          <div class="row align-items-center">
+            
+            <div class="col-11 col-xl-2">
+              <h1 class="mb-0 site-logo"><a href="index.html" class="text-white mb-0"><img src="./assets/media/logo.svg" alt="logo"></a></h1>
+            </div>
+            <div class="col-12 col-md-10 d-none d-xl-block">
+              <nav class="site-navigation position-relative text-right" role="navigation">
 
-              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li class="active"><a href="index.html"><span>Home</span></a></li>
-                <li class="has-children">
-                  <a href="about.html"><span>Dropdown</span></a>
-                  <ul class="dropdown arrow-top">
-                    <li><a href="#">Menu One</a></li>
-                    <li><a href="#">Menu Two</a></li>
-                    <li><a href="#">Menu Three</a></li>
-                    <li class="has-children">
-                      <a href="#">Dropdown</a>
-                      <ul class="dropdown">
-                        <li><a href="#">Menu One</a></li>
-                        <li><a href="#">Menu Two</a></li>
-                        <li><a href="#">Menu Three</a></li>
-                        <li><a href="#">Menu Four</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="listings.html"><span>Listings</span></a></li>
-                <li><a href="about.html"><span>About</span></a></li>
-                <li><a href="blog.html"><span>Blog</span></a></li>
-                <li><a href="contact.html"><span>Contact</span></a></li>
-              </ul>
-            </nav>
-          </div>
+                <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
+                  <li class="active"><a href="index.html"><span>Home</span></a></li>
+                  <li class="has-children">
+                    <a href="about.html"><span>Dropdown</span></a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#">Menu One</a></li>
+                      <li><a href="#">Menu Two</a></li>
+                      <li><a href="#">Menu Three</a></li>
+                      <li class="has-children">
+                        <a href="#">Dropdown</a>
+                        <ul class="dropdown">
+                          <li><a href="#">Menu One</a></li>
+                          <li><a href="#">Menu Two</a></li>
+                          <li><a href="#">Menu Three</a></li>
+                          <li><a href="#">Menu Four</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="listings.html"><span>Listings</span></a></li>
+                  <li><a href="about.html"><span>About</span></a></li>
+                  <li><a href="blog.html"><span>Blog</span></a></li>
+                  <li><a href="contact.html"><span>Contact</span></a></li>
+                </ul>
+              </nav>
+            </div>
 
 
-          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;">
-                <a href="#" class="site-menu-toggle js-menu-toggle text-white">
-                    <span class="icon-menu">
-                        <svg width="20px" height="20px" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-bars fa-w-14 fa-2x"><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z" class=""></path></svg>
-                    </span>
-                </a>
+            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;">
+                  <a href="#" class="site-menu-toggle js-menu-toggle text-white">
+                      <span class="icon-menu">
+                          <svg width="20px" height="20px" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-bars fa-w-14 fa-2x"><path fill="currentColor" d="M442 114H6a6 6 0 0 1-6-6V84a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6zm0 160H6a6 6 0 0 1-6-6v-24a6 6 0 0 1 6-6h436a6 6 0 0 1 6 6v24a6 6 0 0 1-6 6z" class=""></path></svg>
+                      </span>
+                  </a>
+              </div>
+
             </div>
 
           </div>
-
         </div>
-      </div>
-      
-    </header>
+        
+      </header>
+    </div>
 
     
--- index.php
+++ index.php
@@ -1,3 +1,134 @@
 <?php include './includes/header.php';?>
-<br>
+<section class="index-banner-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-wrap justify-content-between">
+                <div class="col-lg-7 mt-5 content-block">
+                    <h3 class="title-content">Choose a trusted paper writing service. Save your time. Score better.</h3>
+                    <p class="subtitle-content">Simply kick back and relax. Grade Miners will take good care of your essays and research papers, while you’re enjoying your day.</p>
+                    <div class="hero-new__illustration">
+                        <img src="./assets/media/hero-top.png" alt="">
+                        <img src="./assets/media/hero-top-leg.png" alt="" class="hero-new__illustration-leg">
+                        <div class="chip ">
+                            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_b)">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M20 40C31.0457 40 40 31.0457 40 20C40 8.95431 31.0457 0 20 0C12.9284 0 6.71401 3.67012 3.15769 9.2095C1.15911 12.3225 0 16.0259 0 20C0 31.0457 8.95431 40 20 40Z" fill="white" fill-opacity="0.8"></path>
+                                    <path d="M38.5 20C38.5 30.2173 30.2173 38.5 20 38.5C9.78273 38.5 1.5 30.2173 1.5 20C1.5 16.3217 2.57194 12.8984 4.41995 10.0199C7.71202 4.89209 13.4604 1.5 20 1.5C30.2173 1.5 38.5 9.78273 38.5 20Z" stroke="#FFAA00" stroke-width="3"></path>
+                                </g>
+                                <path d="M20.0002 31.4287L11.4287 22.8573H17.143V11.4287H22.8573V22.8573H28.5716L20.0002 31.4287Z" fill="#FFAA00"></path>
+                                <defs>
+                                    <filter id="filter0_b" x="-8.15485" y="-8.15485" width="56.3097" height="56.3097" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                        <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+                                        <feGaussianBlur in="BackgroundImage" stdDeviation="4.07742"></feGaussianBlur>
+                                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"></feComposite>
+                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape"></feBlend>
+                                    </filter>
+                                </defs>
+                            </svg>    
+                            <div class="chip__text">Download it!</div>
+                        </div>
+                        <div class="chip chip__animation-second">
+                            <svg width="37" height="26" viewBox="0 0 37 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1.5 1V24.5H36V1M1.5 1H36M1.5 1L18.5 13.5L36 1" stroke="#FFAA00" stroke-width="2"></path>
+                            </svg>    
+                            <div class="chip__text">Hi there!</div>
+                        </div>
+                        <div class="chip chip__animation-third">
+                            <svg width="32" height="42" viewBox="0 0 32 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M23.5 1H1V41H31V9M23.5 1L31 9M23.5 1V9H31" stroke="#FFAA00" stroke-width="2"></path>
+                                <rect x="7" y="14" width="18" height="2" fill="#C4C4C4"></rect>
+                                <rect x="7" y="18" width="18" height="2" fill="#C4C4C4"></rect>
+                                <rect x="7" y="22" width="18" height="2" fill="#C4C4C4"></rect>
+                                <rect x="7" y="26" width="12" height="2" fill="#C4C4C4"></rect>
+                            </svg>    
+                            <div class="chip__text">Work’s Done.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="price-calculator">
+                        <h3 class="text-center">Calculate Your Order</h3>
+                        <div class="form-row m-0">
+                            <div class="form-group w-100">
+                                <select class="form-control" id="select-assign">
+                                    <option value='1'>Assignment</option>
+                                    <option value='2'>Essay</option>
+                                    <option value='3'>Coursework</option>
+                                    <option value='4'>Research Paper</option>
+                                </select>
+                                <ul class="list-unstyled my-2 d-flex flex-wrap justify-content-center">
+                                    <li class="assign-select mx-2" value='1'>Assignment</li>
+                                    <li class="assign-select mx-2" value='2'>Essay</li>
+                                    <li class="assign-select mx-2" value='3'>Coursework</li>
+                                    <li class="assign-select mx-2" value='4'>Research Paper</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="form-row m-0">
+                            <div class="form-group w-100">
+                                <select class="form-control" id="select-level">
+                                    <option value='1'>High School</option>
+                                    <option value='2'>College</option>
+                                    <option value='3'>UnderGraduate</option>
+                                    <option value='4'>Master</option>
+                                    <option value='5'>Ph.D.</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row m-0">
+                            <div class="form-group w-100">
+                                <select class="form-control" id="select-days">
+                                    <option value='1'>1 hour</option>
+                                    <option value='2'>2 hour</option>
+                                    <option value='3'>3 hour</option>
+                                    <option value='4'>6 hour</option>
+                                    <option value='5'>12 hour</option>
+                                    <option value='6'>24 hour</option>
+                                    <option value='7'>48 hour</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row m-0">
+                            <div class="form-group w-100">
+                                <select class="form-control" id="select-pages">
+                                    <option value='1'>1 page</option>
+                                    <option value='2'>2 page</option>
+                                    <option value='3'>3 page</option>
+                                    <option value='4'>4 page</option>
+                                    <option value='5'>5 page</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-row p-3 m-0 standard-price">
+                            <p class="w-100 m-0 text-center">Standard Price : <span class="mx-2">$18.19</span></p>
+                        </div>
+                        <div class="grey-box">
+                            <div class="form-row m-0 price-unlock">
+                                <div class="col-lg-6 d-flex align-items-center">
+                                    <p class="my-2">Special Price (Locked) <br> save on your first order!</p>
+                                </div>
+                                <div class="col-lg-6">
+                                    <p class="my-2 first-order-price">$15.46</p>
+                                </div>
+                            </div>
+                            <div class="form-row m-0" id="unlock-price">
+                                <div class="col-lg-8">
+                                    <input type="email" placeholder="Enter email to unlock.">
+                                </div>
+                                <div class="col-lg-4">
+                                    <button>Unlock</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row m-0">
+                            <div class="form-group w-100 text-center">
+                                <a href="#" class="btn btn-primary p-3">Order Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include './includes/footer.php';?>
